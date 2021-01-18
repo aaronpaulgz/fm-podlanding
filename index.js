33f39034c2a857461dbbd4d35cf87da51b126a5d
Aaron Gonzalez <aarong.1503@gmail.com>
2021-01-17 19:42:13 -0500

    Ready for first revision

--- index.js
+++ index.js
@@ -16,6 +16,31 @@ const mobvalidateMail = () => {
   }
 };
 
-$Mobinput.addEventListener("focus", () => {
-  $Mobinput.nextElementSibling.style.display = "none";
-});
+// $Mobinput.addEventListener("focus", () => {
+//   $Mobinput.nextElementSibling.style.display = "none";
+// });
+
+// Tablet & Desktop
+
+let $btn = document.getElementById("btn");
+let $input = document.getElementById("input");
+let $form = document.getElementById("form");
+
+const validateMail = () => {
+  let $input = document.getElementById("input");
+  if ($input.value.length < 6) {
+    // $Mobinput.nextElementSibling.style.visibility = "visible";
+    $form.insertAdjacentHTML(
+      "afterend",
+      `<div class="warn--container">
+          <p class="warn">Oops! Please check your mail</p>
+      </div>`
+    );
+  } else {
+    setTimeout(location.reload(), 3000);
+  }
+};
+
+// $input.addEventListener("focus", () => {
+//   $form.nextElementSibling.style.display = "none";
+// });
